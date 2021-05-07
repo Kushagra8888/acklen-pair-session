06c0c0fb98171f575dc16bd0ae48309ca362e3bf
Kushagra Sharma <kushagra8888@gmail.com>
2021-05-07 13:19:25 +0000

    fix test description

--- dummy.test.js
+++ dummy.test.js
@@ -4,6 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
 });
 
-test('adds 3 - 1 to equal 2', () => {
+test('subtract 3 - 1 to equal 2', () => {
     expect(subtract(3, 1)).toBe(2);
   });
